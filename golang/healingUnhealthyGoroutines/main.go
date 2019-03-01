3c372568952eac320cee5895338667a47a50e7ad
genchilu <genchilu@gmail.com>
2019-03-02 00:21:26 +0800

    Add go routine pattern: healing unhealthy goroutines

---
+++ golang/healingUnhealthyGoroutines/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type startGoroutineFn func(done <-chan interface{}, pulseInterval time.Duration) (heartbeat <-chan interface{})
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		select {
+		case <-channels[0]:
+		case <-channels[1]:
+		case <-or(append(channels[2:], orDone)...):
+		}
+	}()
+	return orDone
+}
+
+func newSteward(timeout time.Duration, startGoroutine startGoroutineFn) startGoroutineFn {
+	return func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+		heartbeat := make(chan interface{})
+
+		go func() {
+			defer close(heartbeat)
+
+			var wardDone chan interface{}
+			var wardHeartbeat <-chan interface{}
+
+			startWard := func() {
+				wardDone = make(chan interface{})
+				wardHeartbeat = startGoroutine(or(wardDone, done), timeout/2)
+			}
+			startWard()
+
+			pulse := time.Tick(pulseInterval)
+
+		monitorLoop:
+			for {
+				timeoutSignal := time.After(timeout)
+
+				for {
+					select {
+					case <-pulse:
+						select {
+						case heartbeat <- struct{}{}:
+						default:
+						}
+					case <-wardHeartbeat:
+						continue monitorLoop
+					case <-timeoutSignal:
+						log.Println("steward: ward unhealthy; restarting")
+						close(wardDone)
+						startWard()
+						continue monitorLoop
+					case <-done:
+						return
+					}
+				}
+			}
+		}()
+
+		return heartbeat
+	}
+}
+
+func main() {
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	doWork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
+		log.Println("ward: Hello, I'm irresponsible!")
+
+		go func() {
+			<-done
+			log.Println("ward: I am halting.")
+		}()
+
+		return nil
+	}
+
+	doWorkWithSteward := newSteward(4*time.Second, doWork)
+
+	done := make(chan interface{})
+
+	time.AfterFunc(9*time.Second, func() {
+		log.Println("main: halting steward and ward.")
+		close(done)
+	})
+
+	for range doWorkWithSteward(done, 4*time.Second) {
+	}
+
+	log.Println("Done")
+}
